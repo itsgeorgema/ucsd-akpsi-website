3220b561e57a62337ae1a91fa8a9e0ad691dbeb9
itsgeorgema <georgema2020@gmail.com>
2025-08-19 17:17:16 -0700

    disable image transformations by vercel to save on money

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
